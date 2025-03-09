5d588a7547d0f82db509af84007dfa5fbae2b1bb
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2025-03-09 22:42:26 +1100

    Setup ready to start later

--- Day_1/src/main.rs
+++ Day_1/src/main.rs
@@ -1,3 +1,15 @@
+use std::{
+    env,
+    fs::read_to_string
+};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let file_path: &String = &args[1];
+
+    let content: String = read_to_string(file_path)
+        .expect("Error reading file");
+
+    println!("File content: {}", content);
 }
