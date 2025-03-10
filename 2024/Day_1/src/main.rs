dd93a6c3e87ecaf27de147ae266fd1048a3b0ae9
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2025-03-10 21:54:17 +1100

    Day 1 wip

--- 2024/Day_1/src/main.rs
+++ 2024/Day_1/src/main.rs
@@ -1,6 +1,10 @@
 use std::{
     env,
-    fs::read_to_string
+    fs::{
+        File,
+        read_to_string
+    },
+    io::Write
 };
 
 fn main() {
@@ -11,5 +15,32 @@ fn main() {
     let content: String = read_to_string(file_path)
         .expect("Error reading file");
 
-    println!("File content: {}", content);
+    let lines: Vec<&str> = content
+        .split("\n")
+        .collect();
+
+    let mut list1: Vec<i32> = Vec::new();
+    let mut list2: Vec<i32> = Vec::new();
+    let mut sum: i32 = 0;
+
+    for line in lines {
+        let values: Vec<&str> = line
+            .split("   ")
+            .collect();
+
+        list1.push(values[0].trim().parse().unwrap());
+        list2.push(values[1].trim().parse().unwrap());
+    }
+
+    list1.sort();
+    list2.sort();
+
+    for i in 0..list1.len() {
+        sum += (list1[i] - list2[i]).abs();
+    }
+
+    File::create("../output.txt")
+        .expect("Error creating file")
+        .write_all(sum.to_string().as_bytes())
+        .expect("Error writing to file");
 }
